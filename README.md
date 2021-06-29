c415fddbf9161747a36ca1b83f92933b066e3a73
Leonardo Huttner <leonardohuttner@Leonardos-Mac-Pro.local>
2021-06-28 23:49:14 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Linktree - V0.1
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Vercel
+Deployed at vercel app
+
+## Personal Settings
+edit this snippet to customize
+```
+empresa:{
+      nome:'name',
+      slogan:'slogan'
+     },
+     dados:[
+       {
+         nome:'contato 1',
+         descricao:'Entre em contato conosco atravez do contato',
+         icon: '/whatsapp.png',
+         link: 'https://wa.me/message/6Z5UKTEG34ROI1'
+       },
+       {
+        nome:'contato 2',
+        descricao:'descricao 2',
+        icon:'/whatsapp.png',
+        link:'https://wa.me/c/555185261791'
+       }
+     ]
+```
---
+++ src/views/fotos.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="fotos q-pa-md">
+        <h6 style="font-size:1.2rem;">Alguns dos nossos Produtos:</h6>
+    <q-responsive :ratio="1" style="width: 100%; max-width: 100%;">
+        <q-carousel
+            animated
+            arrows
+            navigation
+            infinite
+            height="100vh"
+            autoplay
+            v-model="slide"
+        >
+            <q-carousel-slide :name="1" img-src="/fotos/1.jpg"/>
+            <q-carousel-slide :name="2" img-src="/fotos/2.jpg"/>
+            <q-carousel-slide :name="3" img-src="/fotos/3.jpg"/>
+            <q-carousel-slide :name="4" img-src="/fotos/4.jpg"/>
+            <q-carousel-slide :name="5" img-src="/fotos/5.jpg"/>
+            <q-carousel-slide :name="6" img-src="/fotos/6.jpg"/>
+            <q-carousel-slide :name="7" img-src="/fotos/7.jpg"/>
+            <q-carousel-slide :name="8" img-src="/fotos/8.jpg"/>
+            <q-carousel-slide :name="9" img-src="/fotos/9.jpg"/>
+            <q-carousel-slide :name="10" img-src="/fotos/10.jpg"/>
+            <q-carousel-slide :name="11" img-src="/fotos/11.jpg"/>
+            <q-carousel-slide :name="12" img-src="/fotos/12.jpg"/>
+            <q-carousel-slide :name="13" img-src="/fotos/13.jpg"/>
+            <q-carousel-slide :name="14" img-src="/fotos/14.jpg"/>
+            <q-carousel-slide :name="15" img-src="/fotos/15.jpg"/>
+            <q-carousel-slide :name="16" img-src="/fotos/16.jpg"/>
+        </q-carousel>
+    </q-responsive>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            slide:1
+        }
+    },
+    methods:{
+        automatizaSlide(){
+
+        }
+    }
+}
+</script>
+
+<style>
+    .fotos{
+        height: 100%;
+        margin-bottom: 10px;
+        padding-bottom: 10px;
+    }
+</style>
